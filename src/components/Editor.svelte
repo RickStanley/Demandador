f8983254598e51ca5e9e3f72f676c3e107892cc7
Rick Stanley <rick-stanley@outlook.com>
2020-03-18 14:56:06 -0400

    Licença

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -491,7 +491,7 @@
     </svg>
   </button>
 </div>
-<!-- <aside class="dica-callout unselectable">
+<!-- <aside class="dica-callout">
   <svg xmlns="http://www.w3.org/2000/svg" style="isolation:isolate;" viewBox="0 0 48 48"><path fill="#C0BBCD" fill-rule="nonzero" d="M22 34h4V22h-4v12zm2-30C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 36c-8.82 0-16-7.18-16-16S15.18 8 24 8s16 7.18 16 16-7.18 16-16 16zm-2-22h4v-4h-4v4z"></path></svg>
   <p>Você pode re-ordernar a tabela clicando nos cabeçalhos.</p>
 </aside> -->
